ce88745d5de4e1633f43f5931a492f2ecdcf3f3a
shubhamking12 <singhshubham2805@gmail.com>
2023-08-21 16:02:46 +0530

    commit dom.js file adding dom.js file

---
+++ dom.js
@@ -0,0 +1,7 @@
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor='green';
+items[0].style.fontWeight='bold';
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
