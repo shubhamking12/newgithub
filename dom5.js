0723925512a9b3623b8fcd6f2704fe37fcd2ffdc
shubhamking12 <singhshubham2805@gmail.com>
2023-08-23 18:17:44 +0530

    Add dom5.2 file

--- dom5.js
+++ dom5.js
@@ -1,5 +1,7 @@
 //localStorage.setItem('fname','Shubham');
-let fname_serialized=JSON.stringify(fname);
-localStorage.setItem('fname'),fname_serialized;
-let  fname_deserialized=JSON.parse(localStorage.getItem('fname'));
-console.log(fname_deserialized);
+//let fname_serialized=JSON.stringify(fname);
+//localStorage.setItem('fname'),fname_serialized;
+//let  fname_deserialized=JSON.parse(localStorage.getItem('fname'));
+//console.log(fname_deserialized);
+
+localStorage.removeItem("fname");
