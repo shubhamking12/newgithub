aa3c4d28d8fe700421808958d1b52a30b4837e6e
shubhamking12 <singhshubham2805@gmail.com>
2023-08-23 12:48:50 +0530

    Add dom4.1.js file

--- dom4.js
+++ dom4.js
@@ -1,9 +1,12 @@
 var form=document.getElementById('addForm');
 var itemList=document.getElementById('items');
+var filter=document.getElementById('filter');
 //Form submit event
 form.addEventListener('submit',addItem);
 //Delete event
 itemList.addEventListener('click',removeItem);
+//Filter event
+filter.addEventListener('keyup',filterItems );
 //Add item
 function addItem(e){
     e.preventDefault();
@@ -38,4 +41,24 @@ function removeItem(e){
         }
         }
     }
+
+    //Filter Items
+    function filterItems(e){
+        //convert text to lowercase
+        var  text=e.target.value.toLowerCase();
+        //get lis
+        var items=itemList.getElementsByTagName('li');
+        //convert to an array
+        Array.from(items).forEach(function(item){
+            var itemName=item.firstChild.textContent;
+            if(itemName.toLowerCase().indexOf(text)!=-1){
+                item.style.display='block';
+            }
+            else{
+                item.style.display='none';
+            }
+
+            }
+        }
+    
 
